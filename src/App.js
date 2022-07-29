3f0a42a19097ea468220d4057b6545670e88da12
Mert süha igaç <95320998+MadonisP@users.noreply.github.com>
2022-07-29 11:03:54 +0300

    products update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Register from "./pages/Register";
 import { BrowserRouter as Router , Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
-  const user=true;
+  const user=false;
   return (
     <Router>
       <Routes>
